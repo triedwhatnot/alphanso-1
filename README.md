342443e2ed326f62628b8eb5c295a92710731f82
Shubham Gupta <shubhamgpt1997@gmail.com>
2024-10-30 14:52:26 +0530

    repo setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Alphanso - round 1 : TO-DO APP
+
+## Steps to run the app:
+1. Run command in root directory : "npm run build" 
+2. And then run, "npm run preview"
---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
+
+/*
+
+  1. tasks - add, delete, status, 
+    1.1 filter, search
+    1.2 form validation - empty task - border red and use in-built prompts
+    1.3 can add animation while removing / adding task
+    1.4 push completed task at the bottom of the list
+  2. undo - deleted task can be retrived for first 5 secs
+  3. use local Storage to persist data
+
+
+*/
